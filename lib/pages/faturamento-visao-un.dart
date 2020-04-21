cd29580e2d02b55128858c701049de3dc064ec07
GuibsonKrause <oliveira.guibson@hotmail.com>
2020-04-21 18:28:31 -0300

    Grafico implementado

--- lib/pages/faturamento-visao-un.dart
+++ lib/pages/faturamento-visao-un.dart
@@ -15,11 +15,28 @@ class FaturamentoVisaoUn extends StatefulWidget {
 
 class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
   DateTime _dataInicial, _dataFinal;
+  bool visaoGrafico = false;
+  bool visaoTabela = true;
+  Color corGrafico = Colors.black26;
+  Color corTabela = Color(COR_PRIMARY);
+  List<charts.Series<Task, String>> _seriesPieData;
 
   _generateData() {
     var pieData = [
-      new Task('Matriz', '2.000.000,00', colorVal)
-    ]
+      new Task('Matriz', '2.000.000,00', 33.33, Color(0xffbee8d5)),
+      new Task('Filial A', '1.000.000,00', 16.66, Color(0xffc8d1e1)),
+      new Task('Filial B', '1.500.000,00', 25.00, Color(0xfffef1c9)),
+      new Task('Filial C', '1.500.000,00', 25.00, Color(0xfffce0cb)),
+    ];
+
+    _seriesPieData.add(charts.Series(
+      data: pieData,
+      domainFn: (Task task, _) => task.task,
+      measureFn: (Task task, _) => task.taskPercent,
+      colorFn: (Task task, _) => charts.ColorUtil.fromDartColor(task.colorVal),
+      id: 'Daily task',
+      labelAccessorFn: (Task row, _) => '${row.taskValue}',
+    ));
   }
 
   @override
@@ -28,6 +45,7 @@ class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
 
     _dataInicial = DateTime.now();
     _dataFinal = DateTime.now();
+    _seriesPieData = List<charts.Series<Task, String>>();
     _generateData();
   }
 
@@ -112,7 +130,7 @@ class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
                     selectDate: (DateTime date) {
                       print(date);
                       setState(() {
-                        _dataInicial = date;
+                        _dataFinal = date;
                       });
                     },
                   ),
@@ -142,78 +160,71 @@ class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
             SizedBox(
               height: 13,
             ),
-            SizedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    "Mesmo Período (Ano Anterior):",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Color(0xFF606062)),
-                  ),
-                  SizedBox(
-                    width: 1,
-                    height: 1,
-                  ),
-                  Text(
-                    "%",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Color(0xFF606062)),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                ],
-              ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Mesmo Período (Ano Anterior):",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Color(0xFF606062)),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Text(
+                      "%",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Color(0xFF606062)),
+                    )),
+              ],
             ),
             SizedBox(
-              height: 4,
+              height: 9,
             ),
-            SizedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 260,
-                    decoration: myBoxDecoration(),
-                    child: Center(
-                      child: Text(
-                        "R\$4.000.000,00",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Color(0xFF606062)),
-                        textAlign: TextAlign.center,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Expanded(
+                    flex: 4,
+                    child: Container(
+                      height: 50,
+                      decoration: myBoxDecoration(),
+                      child: Center(
+                        child: Text(
+                          "R\$4.000.000,00",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Color(0xFF606062)),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                  ),
-                  Container(
-                    height: 50,
-                    width: 80,
-                    decoration: myBoxDecoration(),
-                    child: Center(
-                      child: Text(
-                        "33,33",
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20,
-                            color: Color(0XFF00ab85)),
-                        textAlign: TextAlign.center,
+                    )),
+                SizedBox(
+                  width: 13,
+                ),
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 50,
+                      decoration: myBoxDecoration(),
+                      child: Center(
+                        child: Text(
+                          "33,33",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                              color: Color(0XFF00ab85)),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
+                    )),
+              ],
             ),
             SizedBox(
               height: 13,
@@ -241,80 +252,133 @@ class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
                       width: 40,
                     ),
                     IconButton(
-                      icon: Icon(Icons.grid_on, color: Color(COR_PRIMARY)),
-                      onPressed: () {},
+                      icon: Icon(Icons.grid_on, color: corTabela),
+                      onPressed: () {
+                        setState(() {
+                          visaoTabela = true;
+                          visaoGrafico = false;
+                          corTabela = Color(COR_PRIMARY);
+                          corGrafico = Colors.black26;
+                        });
+                      },
                     ),
                     IconButton(
-                      icon: Icon(Icons.donut_large, color: Color(0xFFbdbfc1)),
-                      onPressed: () {},
+                      icon: Icon(Icons.pie_chart, color: corGrafico),
+                      onPressed: () {
+                        setState(() {
+                          visaoGrafico = true;
+                          visaoTabela = false;
+                          corTabela = Colors.black26;
+                          corGrafico = Color(COR_PRIMARY);
+                        });
+                      },
                     ),
                   ],
                 ),
               ),
             ),
-            SizedBox(
-              height: 13,
+            Visibility(
+              visible: visaoTabela,
+              child: SizedBox(
+                height: 340,
+                child: DataTable(
+                  columns: [
+                    DataColumn(
+                        label: Text(
+                      'Und. Neg.',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )),
+                    DataColumn(
+                        label: Text(
+                      '2020 (R\$)',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )),
+                    DataColumn(
+                        label: Text(
+                      '%',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    ))
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(Text('Matriz')),
+                      DataCell(Text('2.000.000,00')),
+                      DataCell(Text('33,33')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Filial A')),
+                      DataCell(Text('1.000.000,00')),
+                      DataCell(Text('16,66')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Filial B')),
+                      DataCell(Text('1.500.000,00')),
+                      DataCell(Text('25,00')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Filial C')),
+                      DataCell(Text('1.500.000,00')),
+                      DataCell(Text('25,00')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Total',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataCell(Text('6.000.000,00',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataCell(Text('100,00',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                    ]),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(
-              height: 150,
-              child: DataTable(
-                columns: [
-                  DataColumn(
-                      label: Text('Und. de Negócio',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 7,
-                            color: Colors.black),
-                        textAlign: TextAlign.center,)),
-                  DataColumn(
-                      label: Text('2020 (R\$)',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                            color: Colors.black),
-                        textAlign: TextAlign.center,)),
-                  DataColumn(
-                      label: Text('%',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                            color: Colors.black),
-                        textAlign: TextAlign.center,))
-                ],
-                rows: [
-                  DataRow(cells: [
-                    DataCell(Text('Matriz')),
-                    DataCell(Text('2.000.000,00')),
-                    DataCell(Text('33,33')),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Text('Filial A')),
-                    DataCell(Text('1.000.000,00')),
-                    DataCell(Text('16,66')),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Text('Filial B')),
-                    DataCell(Text('1.500.000,00')),
-                    DataCell(Text('25,00')),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Text('Filial C')),
-                    DataCell(Text('1.500.000,00')),
-                    DataCell(Text('25,00')),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Text('Total',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                    DataCell(Text('6.000.000,00',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                    DataCell(Text('100,00',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                  ]),
-                ],
+            Visibility(
+              visible: visaoGrafico,
+              child: SizedBox(
+                height: 340,
+                child: charts.PieChart(
+                  _seriesPieData,
+                  animate: true,
+                  animationDuration: Duration(seconds: 1),
+                  behaviors: [
+                    new charts.DatumLegend(
+                        position: charts.BehaviorPosition.bottom,
+                        outsideJustification:
+                            charts.OutsideJustification.startDrawArea,
+                        horizontalFirst: false,
+                        desiredMaxRows: 1,
+                        cellPadding: new EdgeInsets.all(4),
+                        entryTextStyle: charts.TextStyleSpec(
+                            color: charts.MaterialPalette.purple.shadeDefault,
+                            fontFamily: 'Georgia',
+                            fontSize: 11))
+                  ],
+                  defaultRenderer: new charts.ArcRendererConfig(
+                      arcWidth: 100,
+                      arcRendererDecorators: [
+                        new charts.ArcLabelDecorator(
+                            labelPosition: charts.ArcLabelPosition.inside,
+                            insideLabelStyleSpec: charts.TextStyleSpec(
+                              fontSize: 13,
+                            ))
+                      ]),
+                ),
               ),
             ),
             SizedBox(
-              height: 200,
+              height: 13,
             ),
             Container(
               height: 60,
@@ -343,12 +407,11 @@ class _FaturamentoVisaoUnState extends State<FaturamentoVisaoUn> {
   }
 }
 
-class Task{
+class Task {
   String task;
   String taskValue;
   double taskPercent;
   Color colorVal;
 
   Task(this.task, this.taskValue, this.taskPercent, this.colorVal);
-
 }
